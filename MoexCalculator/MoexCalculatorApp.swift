79cd7deb72c1142778e4d8a9db1533c636fe7532
ketamine <vladg3@mail.ru>
2024-03-02 12:40:54 +0700

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Vladislav Mishukov on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
