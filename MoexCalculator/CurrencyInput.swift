7bf4ec68af64a59fc8e111b3fe6da4376184171e
ketamine <vladg3@mail.ru>
2024-03-03 13:40:00 +0700

    Оптимизация кода, выбор валюты

---
+++ MoexCalculator/CurrencyInput.swift
@@ -0,0 +1,18 @@
+//
+//  CurrencyInput.swift
+//  MoexCalculator
+//
+//  Created by Vladislav Mishukov on 03.03.2024.
+//
+
+import SwiftUI
+
+struct CurrencyInput: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrencyInput()
+}
